876f4269f07a0af9192e40c0daba42764a8de11c
ghost1 <sonofagronom@gmail.com>
2021-09-18 00:43:38 +0700

    0.48 added text to main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,52 @@
 <template>
   <div class='main main__container'>
     <div class="main page">
-      Welcome page
+      <div class='page__body'>
+        <div class='page services'>
+          <div class='services'>
+            <div class='services__title'>
+              Услуги
+            </div>
+            <div class='services__desc'>
+              Наша компания предоставляет полный спектр услуг, связанный с погребением, включая оформление документов и доставку.
+              Подробнее, вы можете ознакомиться с ними в разделе "Услуги".
+            </div>
+          </div>
+          <div class='products'>
+            <div class='products__title'>
+              Каталог
+            </div>
+            <div class='products__desc'>
+              Наша компания предлагает огромный ассортимент товаров, связанных с погребением.
+              Подробнее, вы можете ознакомиться с ассортиментов в разделе "Каталог".
+            </div>
+          </div>
+          <div class='contacts'>
+            <div class='contacts__title'>
+              Контакты
+            </div>
+            <div class='contacts__desc'>
+              О том, как связаться с нами вы можете в разделе "Контакты".
+            </div>
+          </div>
+          <div class='contacts'>
+            <div class='contacts__title'>
+              Вакансии
+            </div>
+            <div class='contacts__desc'>
+              Мы всегда заинтересованы в поиске сотрудников. Все актуальные вакансии вы сможете увидеть в разделе "Вакансии".
+            </div>
+          </div>
+          <div class='about'>
+            <div class='about__title'>
+              О нас
+            </div>
+            <div class='about__desc'>
+              Узнать о нашей компании вы сможете в разделе "О нас".
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -16,7 +61,5 @@ export default {}
     height: 100%;
   }
 }
-.page {
-  background: aliceblue;
-}
+.page {}
 </style>
