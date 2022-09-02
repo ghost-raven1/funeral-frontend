41c40cba7214c8a4d6f349b0a3fb82469d9dfd2e
ghost1 <sonofagronom@gmail.com>
2022-09-02 22:32:30 +0700

    master added yandex meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,7 @@ export default {
     title: 'Ритуальные услуги',
     meta: [
       { charset: 'utf-8' },
+      { name: 'yandex-verification', content: '4668475ba6cf5a8f' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
